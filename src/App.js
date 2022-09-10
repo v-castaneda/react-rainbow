743f78ce9fe93c0ba5ca185c0a0a7ab592c402a6
v-castaneda <103009800+v-castaneda@users.noreply.github.com>
2022-09-10 12:49:25 -0400

    added ColorBlock.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
+import React, { useState } from 'react'
 import './App.css';
+import ColorBlock from './components/ColorBlock';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    let colors = [
+        'violet', 'blue', 
+        'lightblue', 'green', 
+        'greenyellow', 'yellow', 
+        'orange', 'red'
+      ]
+
+    let colorMap = colors.map((color, i) => {
+        return (
+            <ColorBlock color={color} />
+        )
+    })
+
+    return (
+        <div className="App">
+            {colors.map((color, i) => 
+                <ColorBlock key={i} color={color} />
+            )}
+        </div>
+    )
 }
 
 export default App;
